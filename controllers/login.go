68c7cd5c040b63e4f6ab2d265538e8a25d78ff02
Rajesh Ghosh <rajeshghosh4007@gmail.com>
2015-02-03 16:13:29 +0600

    login_complete

--- controllers/login.go
+++ controllers/login.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
-	_ "test/models"
+	"test/models"
 )
 
 type LoginController struct {
@@ -15,19 +15,32 @@ func (this *LoginController) Login() {
 }
 
 func (this *LoginController) LoginCheck() {
-	Email := this.GetString("email")
-	Pass := this.GetString("pass")
-	beego.Info(Email, Pass)
+	// Email := this.GetString("email")
+	// Pass := this.GetString("pass")
+	// beego.Info(Email, Pass)
+
+	u := new(models.User)
+	u.Email = this.GetString("email")
+	u.Pass = this.GetString("pass")
+
+	// beego.Info(u.Fname)
+
 	o := orm.NewOrm()
-	qs := o.QueryTable("user")
-	em := qs.Filter("email", "Email")
+	err := o.Read(u, "Email", "Pass")
+	//qs := o.QueryTable("user")
+	//em := qs.Filter("email", "Email")
 	//pas := qs.Filter("pass", "Pass")
 	//beego.Info(em, pas)
-	err := o.Read(&em)
+
 	if err == orm.ErrNoRows {
-		beego.Info("Wrong!")
+		beego.Info("No")
+		this.Redirect("/login", 302)
+		return
 	} else {
-		beego.Info("Yes!")
+		beego.Info("Yes")
+		this.Redirect("/", 302)
+		return
 	}
+	//beego.Info("fgfghfgh")
 
 }
